ce21ab2b795967aede98a6f1c42821364c60fa72
tuongvy2111 <tuongvy2111@gmail.com>
2013-11-13 16:00:50 +0000

    đừng dùng Web Form mà hãy dùng "Web Form using Master Page" để thừa kế giao diện

--- QuanLyBenhVien/QuanLyBenhVien/Site.Master.cs
+++ QuanLyBenhVien/QuanLyBenhVien/Site.Master.cs
@@ -9,9 +9,6 @@ namespace QuanLyBenhVien
 {
     public partial class SiteMaster : System.Web.UI.MasterPage
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
+       
     }
 }
