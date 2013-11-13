81c039c3a18fbab2a89d0d2f1d343be578f65dbf
tuongvy2111 <tuongvy2111@gmail.com>
2013-11-13 09:25:06 +0000

    /*begin*/

---
+++ QuanLyBenhVien/QuanLyBenhVien/Database/CSDL.sql
@@ -0,0 +1,5 @@
+create table sinhvien
+(
+	name varchar(12) not null primary key,
+	hoten varchar(25),
+)
---
+++ QuanLyBenhVien/QuanLyBenhVien/Model/personnel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyBenhVien.Model
+{
+    public class personnel
+    {
+        private String name, regency, birthday, technique, sex, address, people, religion, unitwork;
+        public personnel()
+        {
+        }
+        public Boolean Addnew()
+        {
+            return true;
+        }
+    }
+}
