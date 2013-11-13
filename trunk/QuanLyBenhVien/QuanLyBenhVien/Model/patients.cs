6a4b436d39cab9b392999aaa5273b53118fa0ba7
thanphong.dct@gmail.com <thanphong.dct@gmail.com@975c8060-3662-f0e0-c42f-10fd37ad4598>
2013-11-13 06:37:39 +0000

    begin

---
+++ trunk/QuanLyBenhVien/QuanLyBenhVien/Model/patients.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyBenhVien.Model
+{
+    public class patients
+    {
+        private String name, birthday, age, address, sex, people, religion;
+        public patients()
+        {
+        }
+    }
+}
